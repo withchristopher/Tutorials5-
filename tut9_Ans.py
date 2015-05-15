c4f655d0ef8fc645f7ef117db4a2c72810107999
withchristopher <chris.reviresco@gmail.com>
2015-05-15 13:54:24 +0200

    Friday Commit2

--- tut9_Ans.py
+++ tut9_Ans.py
@@ -3,19 +3,30 @@
 #Question 1: Write a least squares code
 
 import numpy
+from scipy.optimize import leastsq
 from matplotlib import pyplot as plt
-import time
-from numpy import fft,ifft
 
-#Numbers
-x = numpy.random.randn([100])
-y = numpy.arange(100)
-z = numpy.arange(0,5)
-h = numpy.ones([5,5])
+#data points
+N = 1000
+t = numpy.linspace(0, 4*numpy.pi, N)
+data = 3.0*numpy.sin(t+0.001) + 0.5 + numpy.random.randn(N)
 
-#Variables
-mu
-sig
+guess_mean = numpy.mean(data)
+guess_std = 3*numpy.std(data)/(2**0.5)
+guess_phase = 0
 
-#Equations, Guassian
-guassian = (-0.5*(x-mu)**2/(sig)**2)/sqrt(2*numpy.pi()*(sig)**2)
+data_first_guess = guess_std*numpy.sin(t+guess_phase) + guess_mean
+
+# Define the function to optimize, in this case, we want to minimize the difference
+# between the actual data and our "guessed" parameters
+optimize_func = lambda x: x[0]*numpy.sin(t+x[1]) + x[2] - data
+est_std, est_phase, est_mean = leastsq(optimize_func, [guess_std, guess_phase, guess_mean])[0]
+
+# recreate the fitted curve using the optimized parameters
+data_fit = est_std*numpy.sin(t+est_phase) + est_mean
+
+plt.plot(data, '.')
+plt.plot(data_fit, label='after fitting')
+plt.plot(data_first_guess, label='first guess')
+plt.legend()
+plt.show()
